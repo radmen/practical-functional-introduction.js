3ccfaf57dd8d43adb6b8027df1306486d5c0c7a6
Radoslaw Mejer <radmen@desmart.com>
2017-04-14 16:26:52 +0200

    Working example of non-functional version of getScoresSummary

--- index.js
+++ index.js
@@ -9,6 +9,28 @@
  * @return {Object}
  */
 function getScoresSummary(userScores) {
+    const validScores = [];
+
+    for (let i = 0; i < userScores.length; i++) {
+        const item = userScores[i];
+
+        if (item.score >= 50 && item.time >= 30) {
+            validScores.push(item);
+        }
+    }
+
+    let sum = 0;
+    let avgTime = 0;
+
+    for (let i = 0; i < validScores.length; i++) {
+        sum += validScores[i].score;
+        avgTime += validScores[i].time;
+    }
+
+    return {
+        sum,
+        avgTime: avgTime / validScores.length,
+    };
 }
 
 module.exports = { getScoresSummary };
--- test.js
+++ test.js
@@ -6,8 +6,8 @@ const scores = require('./index.js');
 describe('scores', function () {
     it('shoud return summary of users scores', () => {
         const result = scores.getScoresSummary(userScores);
-        const sum = 100 + 45 + 55 + 99 + 74;
-        const avgTime = (35 + 60 + 54 + 74 + 153) / 5;
+        const sum = 100 +55 + 99 + 74;
+        const avgTime = (35 + 54 + 74 + 153) / 4;
 
         assert.deepEqual(result, { sum, avgTime });
     });
